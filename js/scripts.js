18516399393696f0d31e39919425d649a90d98f9
Alex Lauderdale <alauderdale@mac.com>
2012-09-09 14:32:20 -0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+
+$('ul#filters li a').click(function(){
+
+  $('ul#filters li a').removeClass('port-active');
+   $(this).addClass('port-active');
+
+
+
+});
+
+
+});
