c386c9595bf993089a2d7518d5e42d14caee554e
Alex Lauderdale <alauderdale@mac.com>
2012-10-23 18:57:02 -0600

    slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,33 @@
 $(document).ready(function(){
 
+   $('#gallery-1').royalSlider({
+    addActiveClass: true,
+    arrowsNav: false,
+    controlNavigation: 'none',
+    autoScaleSlider: true, 
+    autoScaleSliderWidth: 960,     
+    autoScaleSliderHeight: 650,
+    loop: true,
+    fadeinLoadedSlide: false,
+    globalCaption: true,
+    keyboardNavEnabled: true,
+    block: {
+        		// animated blocks options go gere
+        		moveEffect: 'right'
+        	}
+  });
+
+
 
 $('ul#filters li a').click(function(){
 
   $('ul#filters li a').removeClass('port-active');
    $(this).addClass('port-active');
-
-
-
+   
 });
+
+
+
 
 
 });
