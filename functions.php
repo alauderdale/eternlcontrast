d47c37fe9d6e7a8b80e0a7dbac93868c80eb0f01
Alex Lauderdale <alauderdale@mac.com>
2012-11-03 19:01:48 -0600

    hell ya

--- functions.php
+++ functions.php
@@ -93,219 +93,6 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
      add_action('init', 'my_custom_taxonomies', 0);
      
      
-     
-     
-     
-     //    metaboxez
-     
-     
-     
-     $prefix = 'sic_';
-      
-     $meta_box = array(
-
-        
-        'id' => 'my-meta-box',
-        'title' => 'Portfolio content Thumb',
-        'page' => 'page',
-        'context' => 'normal',
-        'priority' => 'high',
-        'fields' => array(
-
-            /// services
-        
-            array(
-                'name' => 'Hero Sub Text',
-                'desc' => 'add sub text belop title',
-                'id' => 'hero_sub',
-                'type' => 'text',
-                'std' => ''
-            ),   
-                        
-        ),
-
-     	'id' => 'my-meta-box',
-     	'title' => 'Portfolio content Thumb',
-     	'page' => 'portfolio_entry',
-     	'context' => 'normal',
-     	'priority' => 'high',
-     	'fields' => array(
-
-            /// portfolio
-     	
-         	array(
-         		'name' => 'Slider Images',
-         		'desc' => 'Enter the full URL of each image on a separate line.',
-         		'id' => 'slider_text',
-         		'type' => 'textarea',
-         		'std' => ''
-         	),
-         	
-         	
-     		array(
-     			'name' => 'Thumb',
-     			'desc' => 'Select a Thumb',
-     			'id' => 'upload_image',
-     			'type' => 'text',
-     			'std' => ''
-     		),
-     		array(
-     			'name' => '',
-     			'desc' => 'Select an thumb Image',
-     			'id' => 'upload_image_button',
-     			'type' => 'button',
-     			'std' => 'Browse'
-     		),
-     		
-     		array(
-     			'name' => 'blockquote Text',
-     			'desc' => 'add quote',
-     			'id' => 'quote',
-     			'type' => 'text',
-     			'std' => ''
-     		),
-     		
-     		array(
-     			'name' => 'sub Text',
-     			'desc' => 'add sub text',
-     			'id' => 'sub_text',
-     			'type' => 'textarea',
-     			'std' => ''
-     		),
-     		     		
-     	),
-
-        'id' => 'my-meta-box',
-        'title' => 'Hero Sub Text',
-        'page' => 'home_eservice',
-        'context' => 'normal',
-        'priority' => 'high',
-        'fields' => array(
-
-            /// services
-        
-            array(
-                'name' => 'service preview',
-                'desc' => 'add a service preview description',
-                'id' => 'service_preview',
-                'type' => 'textarea',
-                'std' => ''
-            ),
-
-            array(
-                'name' => '',
-                'desc' => 'Select a thumb for this service (for home page)',
-                'id' => 'upload_image_button',
-                'type' => 'button',
-                'std' => 'Browse'
-            ),
-
-            array(
-                'name' => 'Service Thumb',
-                'desc' => 'Select a Thumb',
-                'id' => 'upload_image',
-                'type' => 'text',
-                'std' => ''
-            ),
-                        
-        ),
-
-     );
-     
-     add_action('admin_menu', 'mytheme_add_box');
-     
-     // Add meta box
-     function mytheme_add_box() {
-     	global $meta_box;
-      
-     	add_meta_box($meta_box['id'], $meta_box['title'], 'mytheme_show_box', $meta_box['page'], $meta_box['context'], $meta_box['priority']);
-     }
-      
-     // Callback function to show fields in meta box
-     function mytheme_show_box() {
-     	global $meta_box, $post;
-      
-     	// Use nonce for verification
-     	echo '<input type="hidden" name="mytheme_meta_box_nonce" value="', wp_create_nonce(basename(__FILE__)), '" />';
-      
-     	echo '<table class="form-table">';
-      
-     	foreach ($meta_box['fields'] as $field) {
-     		// get current post meta data
-     		$meta = get_post_meta($post->ID, $field['id'], true);
-      
-     		echo '<tr>',
-     				'<th style="width:20%"><label for="', $field['id'], '">', $field['name'], '</label></th>',
-     				'<td>';
-     		switch ($field['type']) {
-      
-      
-      
-      
-     //If Text		
-     			case 'text':
-     				echo '<input type="text" name="', $field['id'], '" id="', $field['id'], '" value="', $meta ? $meta : $field['std'], '" size="30" style="width:97%" />',
-     					'<br />', $field['desc'];
-     				break;
-      
-      
-     //If Text Area			
-     			case 'textarea':
-     				echo '<textarea name="', $field['id'], '" id="', $field['id'], '" cols="60" rows="4" style="width:97%">', $meta ? $meta : $field['std'], '</textarea>',
-     					'<br />', $field['desc'];
-     				break;
-      
-      
-     //If Button	
-      
-     				case 'button':
-     				echo '<input type="button" name="', $field['id'], '" id="', $field['id'], '"value="', $meta ? $meta : $field['std'], '" />';
-     				break;
-     		}
-     		echo 	'<td>',
-     			'</tr>';
-     	}
-      
-     	echo '</table>';
-     }
-      
-     add_action('save_post', 'mytheme_save_data');
-      
-     // Save data from meta box
-     function mytheme_save_data($post_id) {
-     	global $meta_box;
-      
-     	// verify nonce
-     	if (!wp_verify_nonce($_POST['mytheme_meta_box_nonce'], basename(__FILE__))) {
-     		return $post_id;
-     	}
-      
-     	// check autosave
-     	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-     		return $post_id;
-     	}
-      
-     	// check permissions
-     	if ('page' == $_POST['post_type']) {
-     		if (!current_user_can('edit_page', $post_id)) {
-     			return $post_id;
-     		}
-     	} elseif (!current_user_can('edit_post', $post_id)) {
-     		return $post_id;
-     	}
-      
-     	foreach ($meta_box['fields'] as $field) {
-     		$old = get_post_meta($post_id, $field['id'], true);
-     		$new = $_POST[$field['id']];
-      
-     		if ($new && $new != $old) {
-     			update_post_meta($post_id, $field['id'], $new);
-     		} elseif ('' == $new && $old) {
-     			delete_post_meta($post_id, $field['id'], $old);
-     		}
-     	}
-     }
-     
      function my_admin_scripts() {
      wp_enqueue_script('media-upload');
      wp_enqueue_script('thickbox');
@@ -318,6 +105,218 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
      add_action('admin_print_scripts', 'my_admin_scripts');
      add_action('admin_print_styles', 'my_admin_styles');
     
+$prefix = 'dbt_';
 
+$meta_boxes = array(
+    ///page
+    array(
+        'id' => 'my-meta-box-1',
+        'title' => 'Sub Text For page',
+        'pages' => array('page'), // multiple post types
+        'context' => 'normal',
+        'priority' => 'high',
+        'fields' => array(
+            array(
+                'name' => 'Hero Sub Text',
+                'desc' => 'add sub text belop title',
+                'id' => 'hero_sub',
+                'type' => 'text',
+                'std' => ''
+            )
+        )
+    ),
+    //portfolio entries
+    array(
+        'id' => 'my-meta-box-2',
+        'title' => 'Portfolio Entry Options',
+        'pages' => array('portfolio_entry'), // multiple post types
+        'context' => 'normal',
+        'priority' => 'high',
+        'fields' => array(
+            array(
+                'name' => 'Slider Images',
+                'desc' => 'Enter the full URL of each image on a separate line.',
+                'id' => 'slider_text',
+                'type' => 'textarea',
+                'std' => ''
+            ),
+            array(
+                 'name' => 'Thumb',
+                 'desc' => 'Select a Thumb',
+                 'id' => 'upload_image',
+                 'type' => 'text',
+                 'std' => ''
+            ),
+            array(
+                 'name' => '',
+                 'desc' => 'Select an thumb Image',
+                 'id' => 'upload_image_button',
+                 'type' => 'button',
+                 'std' => 'Browse'
+            ),
+            array(
+                 'name' => 'blockquote Text',
+                 'desc' => 'add quote',
+                 'id' => 'quote',
+                 'type' => 'text',
+                 'std' => ''
+            ),
+            array(
+                 'name' => 'sub Text',
+                 'desc' => 'add sub text',
+                 'id' => 'sub_text',
+                 'type' => 'textarea',
+                 'std' => ''
+            ),
+        )
+    ),
+    //Home Services
+    array(
+        'id' => 'my-meta-box-3',
+        'title' => 'Service Options',
+        'pages' => array('home_eservice'), // multiple post types
+        'context' => 'normal',
+        'priority' => 'high',
+        'fields' => array(
+            array(
+                'name' => 'service preview',
+                'desc' => 'add a service preview description',
+                'id' => 'service_preview',
+                'type' => 'textarea',
+                'std' => ''
+            ),
+            array(
+                'name' => '',
+                'desc' => 'Select a thumb for this service (for home page)',
+                'id' => 'upload_image_button',
+                'type' => 'button',
+                'std' => 'Browse'
+            ),
+            array(
+                'name' => 'Service Thumb',
+                'desc' => 'Select a Thumb',
+                'id' => 'upload_image',
+                'type' => 'text',
+                'std' => ''
+            ),
+        )
+
+    ),
+);
+
+add_action('admin_menu', 'mytheme_add_box');
+
+// Add meta box
+function mytheme_add_box() {
+    global $meta_box;
+    
+    add_meta_box($meta_box['id'], $meta_box['title'], 'mytheme_show_box', $meta_box['page'], $meta_box['context'], $meta_box['priority']);
+}
+
+
+foreach ($meta_boxes as $meta_box) {
+    $my_box = new My_meta_box($meta_box);
+}
+
+class My_meta_box {
+ 
+    protected $_meta_box;
+ 
+    // create meta box based on given data
+    function __construct($meta_box) {
+        $this->_meta_box = $meta_box;
+        add_action('admin_menu', array(&$this, 'add'));
+ 
+        add_action('save_post', array(&$this, 'save'));
+    }
+ 
+    /// Add meta box for multiple post types
+    function add() {
+        foreach ($this->_meta_box['pages'] as $page) {
+            add_meta_box($this->_meta_box['id'], $this->_meta_box['title'], array(&$this, 'show'), $page, $this->_meta_box['context'], $this->_meta_box['priority']);
+        }
+    }
+ 
+    // Callback function to show fields in meta box
+    function show() {
+        global $post;
+ 
+        // Use nonce for verification
+        echo '<input type="hidden" name="mytheme_meta_box_nonce" value="', wp_create_nonce(basename(__FILE__)), '" />';
+ 
+        echo '<table class="form-table">';
+ 
+        foreach ($this->_meta_box['fields'] as $field) {
+            // get current post meta data
+            $meta = get_post_meta($post->ID, $field['id'], true);
+ 
+            echo '<tr>',
+                    '<th style="width:20%"><label for="', $field['id'], '">', $field['name'], '</label></th>',
+                    '<td>';
+            switch ($field['type']) {
+                case 'text':
+                    echo '<input type="text" name="', $field['id'], '" id="', $field['id'], '" value="', $meta ? $meta : $field['std'], '" size="30" style="width:97%" />',
+                        '<br />', $field['desc'];
+                    break;
+                case 'textarea':
+                    echo '<textarea name="', $field['id'], '" id="', $field['id'], '" cols="60" rows="4" style="width:97%">', $meta ? $meta : $field['std'], '</textarea>',
+                        '<br />', $field['desc'];
+                    break;
+                case 'select':
+                    echo '<select name="', $field['id'], '" id="', $field['id'], '">';
+                    foreach ($field['options'] as $option) {
+                        echo '<option', $meta == $option ? ' selected="selected"' : '', '>', $option, '</option>';
+                    }
+                    echo '</select>';
+                    break;
+                case 'radio':
+                    foreach ($field['options'] as $option) {
+                        echo '<input type="radio" name="', $field['id'], '" value="', $option['value'], '"', $meta == $option['value'] ? ' checked="checked"' : '', ' />', $option['name'];
+                    }
+                    break;
+                case 'button':
+                    echo '<input type="button" name="', $field['id'], '" id="', $field['id'], '"value="', $meta ? $meta : $field['std'], '" />';
+                    break;
+            }
+            echo     '<td>',
+                '</tr>';
+        }
+ 
+        echo '</table>';
+    }
+ 
+    // Save data from meta box
+    function save($post_id) {
+        // verify nonce
+        if (!wp_verify_nonce($_POST['mytheme_meta_box_nonce'], basename(__FILE__))) {
+            return $post_id;
+        }
+ 
+        // check autosave
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return $post_id;
+        }
+ 
+        // check permissions
+        if ('page' == $_POST['post_type']) {
+            if (!current_user_can('edit_page', $post_id)) {
+                return $post_id;
+            }
+        } elseif (!current_user_can('edit_post', $post_id)) {
+            return $post_id;
+        }
+ 
+        foreach ($this->_meta_box['fields'] as $field) {
+            $old = get_post_meta($post_id, $field['id'], true);
+            $new = $_POST[$field['id']];
+ 
+            if ($new && $new != $old) {
+                update_post_meta($post_id, $field['id'], $new);
+            } elseif ('' == $new && $old) {
+                delete_post_meta($post_id, $field['id'], $old);
+            }
+        }
+    }
+}
 
 ?>
